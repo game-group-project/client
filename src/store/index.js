b4909238ce81bb38141587b9b8e50793f4f07ce3
ramadesi1 <ramadesy.saragih@gmail.com>
2020-03-19 14:32:24 +0700

    add scoring system

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,33 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-// import axios from 'axios'
+import io from 'socket.io-client'
+const socket = io.connect('http://localhost:3000')
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    socket,
+    name: '',
+    score: 0,
+    players: []
   },
   mutations: {
+    SET_SCORE (state, data) {
+      state.players = [
+        ...state.players.filter(element => element.name !== data.name),
+        data
+      ]
+    }
   },
   actions: {
+    sendScore ({ state, commit }, data) {
+      commit('SET_SCORE', data)
+      state.socket.emit(('send-score', data))
+    },
+    onBroadcastScore ({ commit }, data) {
+      commit('SET_SCORE', data)
+    }
   },
   getters: {
   }
---
+++ src/views/Game.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1>This is the game page</h1>
+    <div class="score">
+      Players : 
+      {{ players }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  created () {
+    this.socket.on('on-player-joined', (data) => {
+      this.$store.dispatch('onPlayerJoined', data)
+    })
+    this.socket.on('on-broadcast-score', (data) => {
+      this.$store.dispatch('onBroadcastScore', data)
+    })
+  },
+  methods: {
+    addScore () {
+      const data = {
+        name: this.name,
+        score: this.score
+      }
+      this.$store.dispatch('sendScore', data)
+    }
+  },
+  computed: {
+    name () {
+      return this.$store.state.name
+    },
+    score () {
+      return this.$store.state.score
+    },
+    socket () {
+      return this.$store.state.socket
+    },
+    players () {
+      return this.$store.state.players
+    }
+  }
+}
+</script>
