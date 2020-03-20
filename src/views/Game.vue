46f53f460369ed80da2eaa322086ca5924143f8c
xavierThufail <xavier.thufail@gmail.com>
2020-03-20 11:05:43 +0700

    change layout game

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,26 +1,41 @@
 <template>
   <div>
-    <h1>This is the game page</h1>
-    <div class="score">
-      Players :
-      <ul>
-        <li v-for="player in players" :key="player.name">{{ `${player.name} | ${player.score}` }}</li>
-      </ul>
-      <button class="btn btn-primary" v-if="isPlaying" @click="restartGame">Restart Game</button>
+    <h1 style="text-align: center" v-if="gameFinished">Game Finished! Waiting other player to finish!</h1>
+    <div class="d-flex align-items-center flex-column">
+      <img src="../assets/logo.png" alt="">
     </div>
-    <div class="row">
+    <div class="d-flex justify-content-between mb-2">
+      <h3 style="margin-right:100px">Players :</h3>
+      <button class="btn btn-primary" v-if="isPlaying" @click="restartGame">Restart Game</button>
       <button v-if="playerCount >= 2 && !isPlaying" @click="startGame" class="btn btn-primary">Start Game</button>
     </div>
+    <div class="d-flex flex-column">
+      <ul class="list-group d-flex flex-row">
+        <li class="list-group-item d-flex align-items-center mr-3 mb-1"
+        v-for="player in players" :key="player.name"
+        >
+          {{ player.name }}
+          <span class="badge badge-primary badge-pill ml-3"> {{ player.score }}</span>
+        </li>
+      </ul>
+    </div>
     <div class="row">
-      <!-- Ini untuk card -->
-      <CardList v-if="isPlaying && !gameFinished"/>
-      <h1 v-if="gameFinished">Game Finished! Waiting other player to finish!</h1>
+      <div class="col">
+        <CardList v-if="isPlaying && !gameFinished"/>
+      </div>
     </div>
     <div class="row" v-if="finishedPlayerCount === playerCount">
-      <h1>Highest Score : </h1>
-      <h2>Name : {{ highestScore.name }}</h2>
-      <h2>Name : {{ highestScore.score }}</h2>
-      <button class="btn btn-primary" @click="restartGame">Restart Game</button>
+      <div class="col">
+        <div v-if="typeof highestScore !== 'string'">
+          <h1>Highest Score : </h1><br><br>
+          <h2>Name : {{ highestScore.name }}</h2><br><br>
+          <h2>Score : {{ highestScore.score }}</h2><br><br>
+        </div>
+        <div v-else>
+          <h1>DRAW</h1>
+        </div>
+        <button class="btn btn-primary" @click="restartGame">Restart Game</button>
+      </div>
     </div>
   </div>
 </template>
@@ -34,7 +49,7 @@ export default {
   created () {
     const player = {
       name: this.name,
-      score: this.score,
+      score: 0,
       isFinished: false
     }
     this.$store.dispatch('joinGame', player)
